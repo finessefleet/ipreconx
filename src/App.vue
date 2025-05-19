12c5de1c6f94b6b89a9ed328257cd135ef19f455
Aryan B <aryanbiju02@gmail.com>
2025-05-19 21:39:08 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import BlackListedIps from '@/components/BlackListedIps.vue'
+</script>
+
+<template>
+  <BlackListedIps/>
+</template>
---
+++ src/components/BlackListedIps.vue
@@ -0,0 +1,3 @@
+<template>
+<h1>IPRECONX</h1>
+</template>
